e2faaa9ac4ec015e21fdfdd546f598064ff3b8c3
Eli Roslöf <eli.roslof@gmail.com>
2023-02-07 16:54:49 +0200

    Initialized materials for the anecdotes set and completed exercise 1.12

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+/**
+ * Chooses a random index. Used for getting a random anecdote.
+ * @param anCount The number of anecdotes the program contains 
+ * @returns A number corresponding to an anecdote
+ */
+const chooseAnecdote = ( anCount ) => {
+  return Math.floor(Math.random() * anCount )
+}
+
+/**
+ * Renders a button.
+ * @param handleClick A method that determines the button's function 
+ * @returns A rendering of the button
+ */
+const Button = ({handleClick}) => (
+  <button onClick={handleClick}>
+    Next anecdote
+  </button>
+)
+
+/**
+ * Acts as the root of the application, and contains all anecdotes.
+ */
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(chooseAnecdote(anecdotes.length))
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={() => setSelected(chooseAnecdote(anecdotes.length))} />
+    </div>
+  )
+}
+
+export default App
