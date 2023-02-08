7a43e790e136b23c75a4e2041ed00481f32f0e47
Eli Roslöf <eli.roslof@gmail.com>
2023-02-08 16:19:18 +0200

    Added completed exercise 2.5.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,25 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ counts }) => <b>Total of exercises {counts.reduce((acc, curr) => acc + curr)}</b>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part => 
-      <Part key={part.id} part={part} />
-      )}   
-  </>
-
-const Course = ({ course }) =>
-  <>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total counts={course.parts.map(part => part.exercises)} />
-  </>
+import Course from "./Course"
 
 const App = () => {
   const courses = [
