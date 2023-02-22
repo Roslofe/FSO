6b0b86b9b54f57700a07d4cda4a283f530ee98e6
Eli Roslöf <eli.roslof@gmail.com>
2023-02-22 10:47:57 +0200

    Added completed exercise 3.3.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -40,6 +40,16 @@ app.get('/info', (request, response) => {
                     )
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = numbers.find(entry => entry.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
