440aa40280d38f2bb58998546e932cd505bdd51d
Eli Roslöf <eli.roslof@gmail.com>
2023-02-22 11:35:00 +0200

    Added completed exercise 3.6.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -57,6 +57,34 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    return Math.floor(Math.random()*10000)
+}
+
+app.post('/api/persons', (request, response) => {
+    console.log('start')
+    const body = request.body
+
+    if(!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name and number must be provided'
+        })
+    }
+    if (numbers.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: Number(body.number),
+    }
+    numbers = numbers.concat(person)
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
