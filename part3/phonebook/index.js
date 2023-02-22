02b8aebaf22f831b0f30e1e3e163f7199dcce94d
Eli Roslöf <eli.roslof@gmail.com>
2023-02-22 10:37:13 +0200

    Added completed.exercise 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -32,6 +32,14 @@ app.get('/api/persons', (request, response) => {
     response.end(JSON.stringify(numbers))
 })
 
+app.get('/info', (request, response) => {
+    const personNum = numbers.length
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${personNum} people</p>
+                    <p>${date}</p>`
+                    )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
