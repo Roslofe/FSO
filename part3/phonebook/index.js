2538d18f546c2fbdd8ad4738d49683613a1200f6
Eli Roslöf <eli.roslof@gmail.com>
2023-03-09 16:08:56 +0200

    Added completed part 3.15.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -67,10 +67,10 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    numbers = numbers.filter(entry => entry.id !== id)
-  
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
 })
 
 const generateId = () => {
