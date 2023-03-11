6d2387f3c34c5ebaafd0bd4eabd1c2fa5c80d91a
Eli Roslöf <eli.roslof@gmail.com>
2023-03-11 19:26:09 +0200

    Added completed exercise 4.13.

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -13,58 +13,73 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('get returns correct blogs', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(2)
-})
+describe('when there is initially some blogs saved', () => {
+  test('get returns correct blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-test('blogs should have id property', async () => {
-  const response = await helper.blogsInDb()
-  expect(response[0].id).toBeDefined()
-})
+  test('blogs should have id property', async () => {
+    const response = await helper.blogsInDb()
+    expect(response[0].id).toBeDefined()
+  })
 
-test('new blogs should be able to be added', async () => {
-  await api.post('/api/blogs')
-    .send(helper.newBlog)
-    .expect(201)
-  const apiBlogs = await helper.blogsInDb()
-  expect(apiBlogs).toHaveLength(3)
-})
+  describe('adding new blogs', () => {
+    test('new blogs should be able to be added', async () => {
+      await api.post('/api/blogs')
+        .send(helper.newBlog)
+        .expect(201)
+      const apiBlogs = await helper.blogsInDb()
+      expect(apiBlogs).toHaveLength(helper.initialBlogs.length + 1)
+    })
 
-test('if likes-property is missing, defaulted to 0', async () => {
-  const newBlog = {
-    title: helper.newBlog.title,
-    author: helper.newBlog.author,
-    url: helper.newBlog.url
-  }
+    test('if likes-property is missing, defaulted to 0', async () => {
+      const newBlog = {
+        title: helper.newBlog.title,
+        author: helper.newBlog.author,
+        url: helper.newBlog.url
+      }
 
-  const addedBlog = await api.post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
+      const addedBlog = await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
 
-  expect(addedBlog.body.likes).toBe(0)
-})
+      expect(addedBlog.body.likes).toBe(0)
+    })
 
-test('adding a blog with no title responds with status 400', async () => {
-  const faultyBlog = {
-    author: helper.newBlog.author,
-    url: helper.newBlog.url
-  }
+    describe('when adding faulty entries', () => {
+      test('adding a blog with no title responds with status 400', async () => {
+        const faultyBlog = {
+          author: helper.newBlog.author,
+          url: helper.newBlog.url
+        }
 
-  await api.post('/api/blogs')
-    .send(faultyBlog)
-    .expect(400)
-})
+        await api.post('/api/blogs')
+          .send(faultyBlog)
+          .expect(400)
+      })
 
-test('adding a blog with no url responds with status 400', async () => {
-  const faultyBlog = {
-    title: helper.newBlog.title,
-    author: helper.newBlog.author
-  }
+      test('adding a blog with no url responds with status 400', async () => {
+        const faultyBlog = {
+          title: helper.newBlog.title,
+          author: helper.newBlog.author
+        }
 
-  await api.post('/api/blogs')
-    .send(faultyBlog)
-    .expect(400)
+        await api.post('/api/blogs')
+          .send(faultyBlog)
+          .expect(400)
+      })
+    })
+  })
+
+  describe('deleting posts', () => {
+    test('blogs can be deleted', async () => {
+      await api.delete(`/api/blogs/${helper.initialBlogs[0]._id}`)
+        .expect(204)
+      const finalBlogs = await helper.blogsInDb()
+      expect(finalBlogs).toHaveLength(helper.initialBlogs.length - 1)
+    })
+  })
 })
 
 afterAll(async () => {
