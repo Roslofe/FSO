4e6119358d234ac590681b9bf41aa3450ac84381
Eli Roslöf <eli.roslof@gmail.com>
2023-03-27 17:59:15 +0300

    Added completed exercise 5.16.

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -25,15 +25,15 @@ const BlogForm = ({ addBlog }) => {
       <form onSubmit={createBlog}>
         <div>
             title:
-          <input type='text' value={title} name='title' onChange={({ target }) => setTitle(target.value)}/>
+          <input className='blog-title' type='text' value={title} name='title' onChange={({ target }) => setTitle(target.value)}/>
         </div>
         <div>
             author:
-          <input type='text' value={author} name='author' onChange={({ target }) => setAuthor(target.value)}/>
+          <input className='blog-author' type='text' value={author} name='author' onChange={({ target }) => setAuthor(target.value)}/>
         </div>
         <div>
             url:
-          <input type='text' value={url} name='url' onChange={({ target }) => setUrl(target.value)}/>
+          <input className='blog-url' type='text' value={url} name='url' onChange={({ target }) => setUrl(target.value)}/>
         </div>
         <button type='submit'>create</button>
       </form>
