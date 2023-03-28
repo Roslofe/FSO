0ebd791d31a11db8378a5d94eb8f78681a32a91c
Eli Roslöf <eli.roslof@gmail.com>
2023-03-28 15:36:27 +0300

    Added completed exercise 5.19.

--- part5/bloglist-frontend/cypress/e2e/blog.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog.cy.js
@@ -29,6 +29,26 @@ describe('Blog app', function() {
       cy.get('#Password').type('wrong password')
       cy.get('#login').click()
       cy.contains('wrong username or password')
+        .and('have.css', 'color', 'rgb(178, 34, 34)')
+    })
+  })
+
+  describe('When logged in', function() {
+    this.beforeEach(function() {
+      cy.request('POST', 'http://localhost:3003/api/login', { username: 'ExamplePerson', password: 'secret' })
+        .then(response => {
+          localStorage.setItem('bloglistUser', JSON.stringify(response.body))
+          cy.visit('http://localhost:3000')
+        })
+    })
+
+    it('A blog can be created', function() {
+      cy.contains('add new blog').click()
+      cy.get('.blog-title').type('Example blog')
+      cy.get('.blog-author').type('Example Author')
+      cy.get('.blog-url').type('notarealblog.com')
+      cy.contains('create').click()
+      cy.contains('Example blog')
     })
   })
   
