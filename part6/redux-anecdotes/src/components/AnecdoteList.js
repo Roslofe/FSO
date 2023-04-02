a87b216ae1827a160ba8bb0ce67535345b49865e
Eli Roslöf <eli.roslof@gmail.com>
2023-04-02 14:05:45 +0300

    Added completed exercise 6.9.

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,9 +2,16 @@ import { useDispatch, useSelector } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
   const dispatch = useDispatch()
-
+  const anecdotes = useSelector(state => {
+    const allAnecdotes = state.anecdotes.sort((a, b) => b.votes - a.votes)
+    if (state.filter === "") {
+      return allAnecdotes
+    } else {
+      return allAnecdotes.filter(a => a.content.includes(state.filter))
+    }
+  })
+  
   return (
     <div>
       {anecdotes.map(anecdote =>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -36,7 +36,7 @@ export const addAnecdote = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
   switch (action.type) {
@@ -55,4 +55,4 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-export default reducer
+export default anecdoteReducer
