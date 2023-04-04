ef5f7e2ca1e9ac9660636b6f6a343b94fabe80ca
Eli Roslöf <eli.roslof@gmail.com>
2023-04-04 20:02:11 +0300

    Added completed exercise 6.22.

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,19 +1,29 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from 'react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useQueryClient, useMutation, QueryClient } from 'react-query'
+import { getAnecdotes, vote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation(vote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    },
+  })
 
   const handleVote = (anecdote) => {
     console.log('vote')
+    const updated = {...anecdote, votes: anecdote.votes + 1}
+    newAnecdoteMutation.mutate(updated)
   }
 
   const result = useQuery(
     'anecdotes',
     getAnecdotes,
     {
-      retry: 1
+      retry: 1,
+      refetchOnWindowFocus: false
     }
   )
   console.log(result)
@@ -33,7 +43,7 @@ const App = () => {
       <Notification />
       <AnecdoteForm />
     
-      {anecdotes.map(anecdote =>
+      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
